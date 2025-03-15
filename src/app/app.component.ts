714bd3a4cb7040b3e22133f905669a1c94ff4700
Javier Barrera <29jbarrera@gmail.com>
2025-03-15 19:13:59 +0100

    separacion componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AsideComponent } from './Components/aside/aside.component';
 import { MainComponent } from './Components/main/main.component';
+import { SkillsComponent } from './Components/skills/skills.component';
+import { ExperienciaComponent } from './Components/experiencia/experiencia.component';
+import { ProyectosComponent } from './Components/proyectos/proyectos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsideComponent, MainComponent],
+  imports: [
+    RouterOutlet,
+    AsideComponent,
+    MainComponent,
+    SkillsComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Portfolio-v2';
-
 }
