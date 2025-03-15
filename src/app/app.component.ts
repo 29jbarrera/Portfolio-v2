e6485166cb5af93e75bb6a88321e1140616e7622
Javier Barrera <29jbarrera@gmail.com>
2025-03-15 11:49:57 +0100

    add parte aside + main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AsideComponent } from './Components/aside/aside.component';
+import { MainComponent } from './Components/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsideComponent, MainComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Portfolio-v2';
