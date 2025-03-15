3cf420903291acbdad1743aad946dfc6c888b2dc
Javier Barrera <29jbarrera@gmail.com>
2025-03-15 17:47:39 +0100

    fix

--- src/app/Components/main/main.component.ts
+++ src/app/Components/main/main.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { FormacionComponent } from '../formacion/formacion.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [FormacionComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
